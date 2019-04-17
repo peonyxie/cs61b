f10645b9aae93504cbb3fbff40aaa97f863fa675
peonyxie <43351530+peonyxie@users.noreply.github.com>
2019-04-16 18:35:53 -0700

    Add files via upload

---
+++ graph-make/Rule.java
@@ -0,0 +1,130 @@
+package make;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static make.Main.error;
+
+/** Represents the rules concerning a single target in a makefile.
+ *  @author P. N. Hilfinger
+ */
+class Rule {
+    /** A new Rule for TARGET. Adds corresponding vertex to MAKER's dependence
+     *  graph. */
+    Rule(Maker maker, String target) {
+        _maker = maker;
+        _depends = _maker.getGraph();
+        _target = target;
+        _vertex = _depends.add(this);
+        _time = _maker.getInitialAge(target);
+        _finished = false;
+    }
+
+    /** Add the target of DEPENDENT to my dependencies. */
+    void addDependency(Rule dependent) {
+        _depends.add(this._vertex, dependent._vertex);
+    }
+
+    /** Add COMMANDS to my command set.  Signals IllegalStateException if
+     *  COMMANDS is non-empty, but I already have a non-empty command set.
+     */
+    void addCommands(List<String> commands) {
+        if (!_commands.isEmpty() && !commands.isEmpty()) {
+            throw new IllegalStateException();
+        } else {
+            for (String i : commands) {
+                _commands.add(i);
+            }
+        }
+    }
+
+    /** Return the vertex representing me. */
+    int getVertex() {
+        return _vertex;
+    }
+
+    /** Return my target. */
+    String getTarget() {
+        return _target;
+    }
+
+    /** Return my target's current change time. */
+    Integer getTime() {
+        return _time;
+    }
+
+    /** Return true iff I have not yet been brought up to date. */
+    boolean isUnfinished() {
+        return !_finished;
+    }
+
+    /** Check that dependencies are in fact built before it's time to rebuild
+     *  a node. */
+    private void checkFinishedDependencies() {
+        for (int i: _depends.successors(this._vertex)) {
+            Rule neighbor = _depends.getLabel(i);
+            if (neighbor.isUnfinished()) {
+                error("Error: not all dependencies are built");
+            }
+        }
+    }
+
+    /** Return true iff I am out of date and need to be rebuilt (including the
+     *  case where I do not exist).  Assumes that my dependencies are all
+     *  successfully rebuilt. */
+    private boolean outOfDate() {
+        if (_time == null) {
+            return true;
+        }
+        for (int i: _depends.successors(this._vertex)) {
+            Rule neighbor = _depends.getLabel(i);
+            if (this._time < neighbor.getTime()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Rebuild me, if needed, after checking that all dependencies are rebuilt
+     *  (error otherwise). */
+    void rebuild() {
+        checkFinishedDependencies();
+
+        if (outOfDate()) {
+            if (_commands.isEmpty()) {
+                error("Error: %s needs to be rebuilt, but has no commands",
+                      _target);
+            } else {
+                for (String i: _commands) {
+                    if (i != null) {
+                        execute(i);
+                    }
+                }
+                _time = _maker.getCurrentTime() + 10;
+            }
+        }
+        _finished = true;
+    }
+
+    /** Execute command.
+     * @param command command.
+     * */
+    void execute(String command) {
+        System.out.println(command);
+    }
+
+    /** The Maker that created me. */
+    private Maker _maker;
+    /** The Maker's dependency graph. */
+    private Depends _depends;
+    /** My target. */
+    private String _target;
+    /** The vertex corresponding to my target. */
+    private int _vertex;
+    /** My command list. */
+    private ArrayList<String> _commands = new ArrayList<>();
+    /** True iff I have been brought up to date. */
+    private boolean _finished;
+    /** My change time, or null if I don't exist. */
+    private Integer _time;
+}
